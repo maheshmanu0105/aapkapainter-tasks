bb069bd257d0563336c508b6f933a2d90c849388
maheshmanu0105 <98731960+maheshmanu0105@users.noreply.github.com>
2022-01-31 13:22:32 +0530

    Update index.php

--- First occurence duplicate of an array/index.php
+++ First occurence duplicate of an array/index.php
@@ -1,1 +1,13 @@
-
+<?php
+function getFirstRepetition($array) {
+	$prev = array();
+	foreach($array as $value) {
+	if (in_array($value, $prev)) {
+    		return $value;
+ 	}
+        $prev[] = $value;
+	}
+        return FALSE;
+}
+var_dump(getFirstRepetition(array(6, 12, 14, 2, 16,14)));
+?>

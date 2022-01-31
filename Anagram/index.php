db50d22064eeadfd8bcabf6076ea0e17beb97d31
maheshmanu0105 <98731960+maheshmanu0105@users.noreply.github.com>
2022-01-31 13:25:30 +0530

    Update index.php

--- Anagram/index.php
+++ Anagram/index.php
@@ -1,1 +1,11 @@
-
+<?php
+function anagram($a, $b) {
+    if (count_chars($a, 1) == count_chars($b, 1)) {
+        return"The two strings are anagram";
+    } else {
+        return"The two strings are not anagram";
+    }
+}
+print_r(anagram('tar', 'rat') . "<br>");
+print_r(anagram('tar', 'bat') . "<br>");
+?>
